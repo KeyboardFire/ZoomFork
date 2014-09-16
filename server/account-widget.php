334453dd932625979cb58e586686fc5ea7ae5e50
KeyboardFire <andy@keyboardfire.com>
2014-09-15 21:17:06 -0500

    server is now closed source

--- server/account-widget.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_SESSION["userid"])) {
-    $uname = htmlspecialchars($_SESSION['username']);
-    echo "Logged in as $uname | <a href='$webroot/logout.php'>Log out</a>";
-} else {
-    echo "<a href='$webroot/login.php'>Log in or register</a>";
-}
--- server/head.php
+++
@@ -1,3 +0,0 @@
-<meta charset='utf-8' />
-<meta name='viewport' content='width=device-width' />
-<link rel='stylesheet' type='text/css' href='<?php echo "$webroot/global.css"; ?>' />
